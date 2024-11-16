8d2c3bfb58ede8964fdbd46aa74ec0b7fd167a74
QQDDTT <hunengwei0414@gmail.com>
2024-11-16 14:56:45 +0900

    20241116

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -9,21 +9,36 @@ APP_NAME="SMS"
 WAR_FILE="SMS.war"
 DOCKER_IMAGE_NAME="sms-image"
 DOCKER_CONTAINER_NAME="sms-container"
+SERVER_PORT=8080
 
-# 第一步：停止并删除已存在的容器
-if [ "$(docker ps -q -f name=$DOCKER_CONTAINER_NAME)" ]; then
-    echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}停止并删除已存在的容器...${NC}"
+# 检查数据库容器是否运行
+if [ "$(docker ps -q -f name=mysql_container)" ]; then
+    echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}数据库容器已运行，跳过启动...${NC}"
+else
+    echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}启动数据库容器...${NC}"
+    docker start mysql_container
+    echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}数据库容器已启动！${NC}"
+fi
+
+# 第一步：停止已存在的容器
+if [ "$(docker container inspect --format '{{.State.Status}}' $DOCKER_CONTAINER_NAME)" ]; then
+    echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}停止已存在的容器...${NC}"
     docker stop $DOCKER_CONTAINER_NAME
+fi
+
+# 第二步：删除已存在的容器
+if [ "$(docker ps -a -f name=$DOCKER_CONTAINER_NAME)" ]; then
+    echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}删除已存在的容器...${NC}"
     docker rm $DOCKER_CONTAINER_NAME
 fi
 
-# 第二步：删除已存在的镜像
+# 第三步：删除已存在的镜像
 if [ "$(docker images -q $DOCKER_IMAGE_NAME)" ]; then
     echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}删除已存在的镜像...${NC}"
     docker rmi -f $DOCKER_IMAGE_NAME
 fi
 
-# 第三步：Maven 打包
+# 第四步：Maven 打包
 echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}开始 Maven 打包...${NC}"
 mvn clean package
 
@@ -34,7 +49,7 @@ fi
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}Maven 打包成功！${NC}"
 
-# 第四步：构建 Docker 镜像
+# 第五步：构建 Docker 镜像
 echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}构建 Docker 镜像...${NC}"
 docker build -t $DOCKER_IMAGE_NAME .
 
@@ -45,10 +60,10 @@ fi
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}Docker 镜像构建成功！${NC}"
 
-# 第五步：创建 Docker 容器
+# 第六步：创建 Docker 容器
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}创建 Docker 容器...${NC}"
-docker run -d --name $DOCKER_CONTAINER_NAME -p 8080:8080 --add-host=localhost:$IP_ADDRESS $DOCKER_IMAGE_NAME
+docker run -d --name $DOCKER_CONTAINER_NAME -p $SERVER_PORT:$SERVER_PORT --add-host=localhost:$IP_ADDRESS $DOCKER_IMAGE_NAME
 
 if [ $? -ne 0 ]; then
     echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}Docker 容器创建失败！${NC}"
@@ -57,9 +72,7 @@ fi
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}Docker 容器创建成功！${NC}"
 
-# 第六步：执行容器
-echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}执行 Docker 容器...${NC}"
-docker exec -d -it $DOCKER_CONTAINER_NAME /bin/bash
+echo "$(date +'%Y-%m-%d %H:%M:%S') ${ORANGE}部署完成！${NC}"
 
 # 等待容器启动
 echo "${ORANGE}等待响应${NC}"
@@ -69,12 +82,11 @@ while [ $WAIT_TIME -le $MAX_TIME ]; do
     sleep 1
     # 撤销上一行内容
     echo "\033[A\033[K${ORANGE}等待中... ${WAIT_TIME} 秒 ${NC}"
-    # 检查 http://localhost:8080/ 是否有响应
-    if curl -s http://localhost:8080/ > /dev/null; then
+    if curl -s http://localhost:$SERVER_PORT/ > /dev/null; then
         echo "${ORANGE}启动成功${NC}"
         # 打开链接
         echo "${ORANGE}打开浏览器${NC}"
-        explorer.exe http://localhost:8080/
+        explorer.exe http://localhost:$SERVER_PORT/
         break
     fi
     WAIT_TIME=$((WAIT_TIME+1))
